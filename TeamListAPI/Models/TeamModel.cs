e4af659fbfe62f2b7c75ee464cc0588895328ea2
jakegalligan <galligan1996@gmail.com>
2019-10-08 14:52:53 -0400

    Add validation for required properties for both players and teams

--- TeamListAPI/Models/TeamModel.cs
+++ TeamListAPI/Models/TeamModel.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace TeamListAPI.Models
 {
     public class Team
     {
         public long Id { get; set; }
+        [Required(ErrorMessage ="Name is required")]
         public string Name { get; set; }
         public string Location { get; set; }
         public List<Player> Players { get; set; }
