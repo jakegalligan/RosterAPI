e2eb8b9fe967a9ecea77bb19c41f343353e6ac3a
jakegalligan <galligan1996@gmail.com>
2019-10-08 15:32:14 -0400

    Allow for player to be created

--- TeamListAPI/Controllers/TeamListController.cs
+++ TeamListAPI/Controllers/TeamListController.cs
@@ -23,11 +23,20 @@ namespace TeamListAPI.Controllers
         [HttpPost("team")]
         public async Task<ActionResult<Team>> CreateTeam(Team team)
         {
+            //save team to teams set in teamlistcontext 
             context.Teams.Add(team);
             await context.SaveChangesAsync();
             return team;
 
         }
-
+        //post route to create a player
+        [HttpPost("player")]
+        public async Task<ActionResult<Player>> CreatePlayer(Player player)
+        {
+            //save player to players set in playerlistcontext 
+            context.Players.Add(player);
+            await context.SaveChangesAsync();
+            return player;
+        }
     }
 }
